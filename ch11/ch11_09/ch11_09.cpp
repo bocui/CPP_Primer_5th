8d3a47e16f24b7753aabddea95d09e6c2d39fa9a
bocui <cuishaobo16@outlook.com>
2017-08-20 16:10:08 +0800

    initial commit

---
+++ ch11/ch11_09/ch11_09.cpp
@@ -0,0 +1,14 @@
+/*
+ Define a map that associates words with a list of line numbers on which the word might occur.
+key: string   value: list<size_t>
+*/
+#include <iostream>
+#include <map>
+#include <string>
+#include <list>
+
+int main() 
+{
+    std::map<std::string, std::list<std::size_t>> mListNoLine;
+}
+
